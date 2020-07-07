10ae160b52ed8a11cf9f3d8def10b458d06c67ab
ntonjeta <ntonjeta@autistici.org>
2020-07-07 18:54:27 +0200

    Add test with random number, but solution is in tests.

--- FizzBuzz.Test/Tests.fs
+++ FizzBuzz.Test/Tests.fs
@@ -3,21 +3,40 @@ module Tests
 open Expecto
 open FizzBuzz
 
+
+let rnd = System.Random()
+let genRandomNumbers count = List.init count (fun _ -> rnd.Next())
+let randomList = genRandomNumbers 100
+
+let actualList =
+    randomList |> List.map (FizzBuzz.fizzbuzz)
+
+let expectedList =
+    randomList
+    |> List.map (fun i ->
+        match i with
+        | n when i % 3 = 0 && i % 5 = 0 -> "FizzBuzz"
+        | n when i % 3 = 0 -> "Fizz"
+        | n when i % 5 = 0 -> "Buzz"
+        | _ -> string i)
+
 [<Tests>]
 let tests =
-  testList "Example tests" [
-    testCase "Three should be Fizz" <| fun _ ->
-      Expect.equal (FizzBuzz.fizzbuzz 3) "Fizz" "Is not Fizz."
+    testList "Example tests"
+        [ testCase "Three should be Fizz"
+          <| fun _ -> Expect.equal (FizzBuzz.fizzbuzz 3) "Fizz" "Is not Fizz."
 
-    testCase "Five should be Buzz" <| fun _ ->
-      Expect.equal (FizzBuzz.fizzbuzz 5) "Buzz" "Is not Buzz."
+          testCase "Five should be Buzz"
+          <| fun _ -> Expect.equal (FizzBuzz.fizzbuzz 5) "Buzz" "Is not Buzz."
 
-    testCase "Nine should be Fizz" <| fun _ ->
-      Expect.equal (FizzBuzz.fizzbuzz 9) "Fizz" "Is not Fizz."
+          testCase "Nine should be Fizz"
+          <| fun _ -> Expect.equal (FizzBuzz.fizzbuzz 9) "Fizz" "Is not Fizz."
 
-    testCase "Twentyi-Five should be Buzz" <| fun _ ->
-      Expect.equal (FizzBuzz.fizzbuzz 25) "Buzz" "Is not Buzz."
+          testCase "Twentyi-Five should be Buzz"
+          <| fun _ -> Expect.equal (FizzBuzz.fizzbuzz 25) "Buzz" "Is not Buzz."
 
-    testCase "Fifteen should be FizzBuzz" <| fun _ ->
-      Expect.equal (FizzBuzz.fizzbuzz 15) "FizzBuzz" "Is not FizzBuzz."
-  ]
+          testCase "Fifteen should be FizzBuzz"
+          <| fun _ -> Expect.equal (FizzBuzz.fizzbuzz 15) "FizzBuzz" "Is not FizzBuzz."
+
+          testCase "Random number test"
+          <| fun _ -> Expect.sequenceEqual actualList expectedList "Not equal" ]
--- FizzBuzz/FizzBuzz.fs
+++ FizzBuzz/FizzBuzz.fs
@@ -4,8 +4,8 @@ module FizzBuzz  =
     let fizzbuzz (x:int) = 
         match x with 
         | 3 -> "Fizz"
+        | 5 -> "Buzz"
         | 9 -> "Fizz"
         | 15 -> "FizzBuzz"
-        | 5 -> "Buzz"
         | 25 -> "Buzz"
         | _ -> string x
